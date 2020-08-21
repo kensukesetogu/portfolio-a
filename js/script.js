e484609008cc826ce28ddf72cea8deaa60b06e0a
kensukesetogu <69514635+kensukesetogu@users.noreply.github.com>
2020-08-21 16:25:42 +0900

    portfolio

---
+++ js/script.js
@@ -0,0 +1,50 @@
+//スティッキーヘッダー
+$(function(){
+    var $win = $(window),
+    $fv = $('.fv'),
+    $header = $('.header'),
+    fvHeight = $fv.outerHeight();
+    fixedClass = 'fixed';
+
+    $win.on('load scroll',function(){
+        var value = $(this).scrollTop();
+        if($win.width() > 768){
+            if(value > fvHeight){
+                $header.addClass(fixedClass);
+            }else{
+                $header.removeClass(fixedClass);
+            }
+        }
+            });
+    });
+
+    //スライダー
+    $('.slider').slick({
+autoplay:true,
+autolayspeed:20000,
+speed: 1000,
+fade: true,
+cssEase:'linear'
+    });
+
+//ハンバーガーメニュー
+$('.burger-btn').on('click',function(){
+$('.header-nav').fadeToggle(300);
+$(this).toggleClass('cross');
+$('body').toggleClass('noscroll');
+
+});
+
+
+
+
+          
+        
+
+
+
+
+
+
+
+
